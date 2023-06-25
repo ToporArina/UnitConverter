1512a5237d8def7afcaca464cc6043f08de52b4d
l000ps <toporoleksandr@gmail.com>
2023-06-25 21:17:32 +0300

    Stage 5/5: Is it getting hot here?

--- Main.kt
+++ Main.kt
@@ -6,23 +6,36 @@ import kotlin.system.exitProcess
 var userInput = ""
 
 enum class Types {
-    LENGTH, WEIGHT
+    LENGTH, WEIGHT, TEMPERATURE
 }
 
-enum class Units(val type: Types, val unit: Double, val short: String, val single: String, val multi: String) {
-    METER(Types.LENGTH, 1.0, "m", "meter", "meters"),
-    KILOMETER(Types.LENGTH, 1000.00, "km", "kilometer", "kilometers"),
-    MILLIMETER(Types.LENGTH, 0.001, "mm", "millimeter", "millimeters"),
-    CENTIMETER(Types.LENGTH, 0.01, "cm", "centimeter", "centimeters"),
-    MILE(Types.LENGTH, 1609.35, "mi", "mile", "miles"),
-    YARD(Types.LENGTH, 0.9144, "yd", "yard", "yards"),
-    FOOT(Types.LENGTH, 0.3048, "ft", "foot", "feet"),
-    INCH(Types.LENGTH, 0.0254, "in", "inch", "inches"),
-    GRAM(Types.WEIGHT, 1.0, "g", "gram", "grams"),
-    KILOGRAM(Types.WEIGHT, 1000.0, "kg", "kilogram", "kilograms"),
-    MILLIGRAMS(Types.WEIGHT, 0.001, "mg", "milligram", "milligrams"),
-    POUNDS(Types.WEIGHT, 453.592, "lb", "pound", "pounds"),
-    OUNCES(Types.WEIGHT, 28.3495, "oz", "ounce", "ounces")
+enum class Units(
+    val type: Types,
+    val from: (Double) -> Double,
+    val to: (Double) -> Double,
+    val list: List<String>
+) {
+    METER(Types.LENGTH, { it }, { it }, listOf("m", "meter", "meters")),
+    KILOMETER(Types.LENGTH, { it * 1000 }, { it / 1000 }, listOf("km", "kilometer", "kilometers")),
+    MILLIMETER(Types.LENGTH, { it * 0.001 }, { it / 0.001 }, listOf("mm", "millimeter", "millimeters")),
+    CENTIMETER(Types.LENGTH, { it * 0.01 }, { it / 0.01 }, listOf("cm", "centimeter", "centimeters")),
+    MILE(Types.LENGTH, { it * 1609.35 }, { it / 1609.35 }, listOf("mi", "mile", "miles")),
+    YARD(Types.LENGTH, { it * 0.9144 }, { it / 0.9144 }, listOf("yd", "yard", "yards")),
+    FOOT(Types.LENGTH, { it * 0.3048 }, { it / 0.3048 }, listOf("ft", "foot", "feet")),
+    INCH(Types.LENGTH, { it * 0.0254 }, { it / 0.0254 }, listOf("in", "inch", "inches")),
+    GRAM(Types.WEIGHT, { it }, { it }, listOf("g", "gram", "grams")),
+    KILOGRAM(Types.WEIGHT, { it * 1000.0 }, { it / 1000.0 }, listOf("kg", "kilogram", "kilograms")),
+    MILLIGRAMS(Types.WEIGHT, { it * 0.001 }, { it / 0.001 }, listOf("mg", "milligram", "milligrams")),
+    POUNDS(Types.WEIGHT, { it * 453.592 }, { it / 453.592 }, listOf("lb", "pound", "pounds")),
+    OUNCES(Types.WEIGHT, { it * 28.3495 }, { it / 28.3495 }, listOf("oz", "ounce", "ounces")),
+    CELSIUS(Types.TEMPERATURE, { it }, { it }, listOf("c", "degree Celsius", "degrees Celsius", "dc", "celsius")),
+    FAHRENHEIT(
+        Types.TEMPERATURE,
+        { (it - 32.0) * (5.0 / 9) },
+        { (it * (9.0 / 5) + 32.0) },
+        listOf("f", "degree Fahrenheit", "degrees Fahrenheit", "df", "fahrenheit")
+    ),
+    KELVIN(Types.TEMPERATURE, { it - 273.15 }, { it + 273.15 }, listOf("k", "kelvin", "kelvins"))
 }
 
 fun main() {
@@ -32,30 +45,54 @@ fun main() {
         if (userInput == "exit") {
             exitProcess(0)
         }
-        val inp = userInput.lowercase().split(" ")
+        var inp = listOf<String>(" ", " ", " ", " ")
+        try {
+            splitUserInput(userInput)[0].toDouble()
+        } catch (e: Exception) {
+            println("Parse error")
+            println()
+            continue
+        }
+        inp = splitUserInput(userInput)
         val from = inp[1]
         val to = inp[3]
-        if (findUnitByValue(from)?.type == findUnitByValue(to)?.type && Types.values().contains(findUnitByValue(from)?.type)) {
+        if (findUnitByValue(from)?.type == findUnitByValue(to)?.type && Types.values()
+                .contains(findUnitByValue(from)?.type)
+        ) {
+            if (inp[0].toDouble() < 0 && findUnitByValue(from)?.type != Types.TEMPERATURE) {
+                if (findUnitByValue(from)?.type == Types.WEIGHT) {
+                    println("Weight shouldn't be negative")
+                    continue
+                }
+                if (findUnitByValue(from)?.type == Types.LENGTH) {
+                    println("Length shouldn't be negative.")
+                    continue
+                }
+            }
             println(
                 "${inp[0].toDouble()} ${
-                    if (inp[0].toDouble() == 1.0) findUnitByValue(from)?.single else findUnitByValue(
+                    if (inp[0].toDouble() == 1.0) findUnitByValue(from)?.list?.get(1) else findUnitByValue(
                         from
-                    )?.multi
-                } is ${inp[0].toDouble() * findUnitByValue(from)?.unit!! / findUnitByValue(to)?.unit!!} ${
-                    if (inp[0].toDouble() * findUnitByValue(from)?.unit!! / findUnitByValue(to)?.unit!! == 1.0) {
-                        findUnitByValue(to)?.single
+                    )?.list?.get(2)
+                } is ${calc(inp[0], findUnitByValue(from)!!.from, findUnitByValue(to)!!.to)} ${
+                    if (calc(inp[0], findUnitByValue(from)!!.from, findUnitByValue(to)!!.to) == 1.0) {
+                        findUnitByValue(to)?.list?.get(1)
                     } else {
-                        findUnitByValue(to)?.multi
+                        findUnitByValue(to)?.list?.get(2)
                     }
                 }"
             )
         } else {
             println(
-                "Conversion from ${if (findUnitByValue(from)?.multi == null) "???" else findUnitByValue(from)?.multi} to ${
+                "Conversion from ${
+                    if (findUnitByValue(from)?.list?.get(2) == null) "???" else findUnitByValue(from)?.list?.get(
+                        2
+                    )
+                } to ${
                     if (findUnitByValue(
                             to
-                        )?.multi == null
-                    ) "???" else findUnitByValue(to)?.multi
+                        )?.list?.get(2) == null
+                    ) "???" else findUnitByValue(to)?.list?.get(2)
                 } is impossible "
             )
         }
@@ -63,8 +100,32 @@ fun main() {
     }
 }
 
+fun calc(value: String, from: (Double) -> Double, to: (Double) -> Double): Double {
+    return to(from(value.toDouble()))
+}
+
+fun splitUserInput(inp: String): List<String> {
+    val splt = mutableListOf<String>()
+    val splitByToOrIn = inp.split("( to | in | convertTo )".toRegex())
+    val firstPart = splitByToOrIn[0].trim().split(" ")
+    val secPart = splitByToOrIn[1].trim().split(" ")
+    if (firstPart.size == 3) {
+        splt.add(firstPart[0])
+        splt.add("${firstPart[1]} ${firstPart[2]}")
+    } else {
+        splt.addAll(firstPart)
+    }
+    splt.add("to")
+    if (secPart.size == 2) {
+        splt.add("${secPart[0]} ${secPart[1]}")
+    } else {
+        splt.addAll(secPart)
+    }
+    return splt
+}
+
 fun findUnitByValue(value: String): Units? {
     return Units.values().find { unit ->
-        unit.short == value || unit.single == value || unit.multi == value
+        unit.list.map { it.lowercase() }.contains(value.lowercase())
     }
 }
